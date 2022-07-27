3256616c5b32b25b256220ba471ad2d35d9d21cc
ttx89 <91700476+ttx89@users.noreply.github.com>
2022-07-26 23:49:51 -0400

    updated register page by adding user role, updated home redirection url after register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -61,6 +61,18 @@
                             </div>
                         </div>
 
+                        <div class="row mb-3">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('User Role') }}</label>
+
+                            <div class="col-md-6">
+                                <select id="role_as" class="form-control" name="role_as" >
+                                    <option value="">--- Choose a user role ---</option>
+                                    <option value="0">customer</option>
+                                    <option value="1">admin</option>
+                                </select>
+                            </div>
+                        </div>
+
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
