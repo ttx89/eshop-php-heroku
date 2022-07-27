26870a6f53014b7ac96f84481aa6b2aff5cdc8a8
ttx89 <91700476+ttx89@users.noreply.github.com>
2022-07-27 10:45:06 -0400

    try again redirection after register, using authentication

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -8,7 +8,6 @@ use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Facades\Auth;
 
 class RegisterController extends Controller
 {
